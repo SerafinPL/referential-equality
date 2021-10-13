e6fa4de3e1180a676dafad8ef8f2dc25b99c8920
serafinpl <ser.burberibar@gmail.com>
2021-10-13 16:47:45 +0200

    full stage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import Container from './Task1/ComponetToTask1';
+
 function App() {
-  return <div>HOOKS WORKSHOP</div>;
+  return (
+    <div>
+      HOOKS WORKSHOP
+      <div><Container /> </div>
+    </div>
+  );
 }
 
 export default App;
+
+
+
